f90abd49d030c5005e687f5dec315c78af24bc61
VittorNCosta <vittor.hy@gmail.com>
2025-11-20 19:00:31 -0300

    criado crud users

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,16 +1,53 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/users_model");
+const {getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} = require("../models/users_model");
+
 
 // rota para listar todos os usuários
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find(); // consulta Mongoose
+    const users = await getAllUsers(); // consulta Mongoose
     res.render("templates/home", { data: users });
   } catch (err) {
     console.error("Erro ao carregar usuários:", err);
     res.status(500).send("Erro ao carregar a página");
   }
 });
+
+
+//Atualiza
+app.put("/users/:id", async (req, res) => {
+  try {
+    const result = await updateUser(req.params.id, req.body);
+    res.json({ message: "Usuário atualizado!", result });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao atualizar." });
+  }
+});
+
+// CRIAR
+router.post("/", async (req, res) => {
+  try {
+    const result = await createUser(req.body);
+    res.json({ message: "Usuário criado!", result });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao criar usuário." });
+  }
+});
+
+// DELETAR
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await deleteUser(req.params.id);
+    res.json({ message: "Usuário deletado!", result });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao deletar." });
+  }
+});
+
 
 module.exports = router;
--- models/users_model.js
+++ models/users_model.js
@@ -1,5 +1,17 @@
 const db = require("../config"); // importa conexão
 
+async function createUser(data) {
+  try {
+    const collection = db.collection("users");
+    const result = await collection.insertOne(data);
+    return result;
+  } catch (err) {
+    console.error("Erro ao criar usuário:", err);
+    throw err;
+  }
+}
+
+
 async function getAllUsers() {
   try {
     const collection = db.collection("users");
@@ -11,4 +23,36 @@ async function getAllUsers() {
   }
 }
 
-module.exports = { getAllUsers };
+
+async function updateUser(id, data) {
+  try {
+    const collection = db.collection("users");
+
+    const result = await collection.updateOne(
+      { _id: new require("mongodb").ObjectId(id) },
+      { $set: data }
+    );
+
+    return result;
+  } catch (err) {
+    console.error("Erro ao atualizar usuário:", err);
+    throw err;
+  }
+}
+
+async function deleteUser(id) {
+  try {
+    const collection = db.collection("users");
+
+    const result = await collection.deleteOne({
+      _id: new ObjectId(id)
+    });
+
+    return result;
+  } catch (err) {
+    console.error("Erro ao deletar usuário:", err);
+    throw err;
+  }
+}
+
+module.exports = { getAllUsers, updateUser, createUser, deleteUser };
